0f5f78260e9e97add1d082b02c185e30e6982df8
Aamir Ayaan Quraishi <aamir.quraishi.btech2023@sitpune.edu.in>
2024-09-26 11:49:59 +0530

    Create display_pointers.cpp

---
+++ display_pointers.cpp
@@ -0,0 +1,25 @@
+//Aamir Ayaan Quraishi
+//23070123002
+//AIM - to store the values using pointers and printing the pointers address and value.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 100;
+    int *aptr;
+    aptr = &a;
+
+    cout << *aptr << endl;
+
+    cout << &a << endl;
+    cout << aptr << endl;
+
+    return 0;
+}
+
+/*
+OUTPUT
+100
+0x61ff08
+0x61ff08
+*/
